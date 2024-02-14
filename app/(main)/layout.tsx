1bb77d4510c439d9fae0558acdf052383d691273
Tony Kim <doyo909@gmail.com>
2024-02-14 11:41:33 -0600

    search function

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,6 +6,7 @@ import { Spinner } from "@/components/spinner";
 import { redirect } from "next/navigation";
 
 import { Navigation } from "./_components/navigation";
+import { SearchCommand } from "@/components/search-command";
 const MainLayout = ({
     children
 }: {
@@ -29,7 +30,8 @@ const MainLayout = ({
     return ( 
         <div className="h-full flex dark:bg[#1F1F1F]">
             <Navigation />
-            <main className="flex-1 h-full overflow-y-auto">  
+            <main className="flex-1 h-full overflow-y-auto">
+                <SearchCommand />
                 {children}
             </main>
             
